cdfee51d34357cd516e5583e5f6a2064fdbb4410
swarup <swarup12x@gmail.com>
2016-10-19 13:55:36 +0530

    # food home delivery system

---
+++ FoodHomeDeliverySystem/src/service/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author swarup
+ *
+ */
+package service;
+
+/**
+ * all DAO classes is calling through respective service classes from respective controller/view model 
+ * 
+ * 
+ */
